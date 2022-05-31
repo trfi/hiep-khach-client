c51a90bca10d463b22736164e5fbbffad3e6dbfa
trfi <truongbuidp@gmail.com>
2022-05-31 12:46:04 +0700

    update

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,7 +4,104 @@ import { NextPageWithLayout } from '@/models'
 
 const Dashboard: NextPageWithLayout = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col w-full items-center justify-center gap-4 mt-10'>
+      
+      <div className="stats bg-primary text-primary-content">
+        <div className="stat">
+          <div className="stat-figure text-info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Downloads</div>
+          <div className="stat-value">12K</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">New Users</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">New Registers</div>
+          <div className="stat-value">430</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+      <div className="stats bg-primary text-primary-content">
+        <div className="stat">
+          <div className="stat-title">Account balance</div>
+          <div className="stat-value">$0,00</div>
+          <div className="stat-actions">
+            <button className="btn btn-success btn-sm">Add funds</button>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Current balance</div>
+          <div className="stat-value">$0,00</div>
+          <div className="stat-actions space-x-2">
+            <button className="btn btn-sm">Withdrawal</button>
+            <button className="btn btn-sm">deposit</button>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total Deposit</div>
+          <div className="stat-value">$0,00</div>
+          <div className="stat-actions">
+            <button className="btn btn-sm">DETAIL</button>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Commissions</div>
+          <div className="stat-value">$0,00</div>
+          <div className="stat-actions">
+            <button className="btn btn-success btn-sm">Withdrawal</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark': '#19232a',
-        'darken': '#161f26',
+        dark: '#19232a',
+        darken: '#161f26',
         'pink-1': '#FBEAEB',
         'blue-1': '#2F3C7E',
         'yellow-1': '#FFB800',
@@ -15,12 +15,12 @@ module.exports = {
         'black-1': '#292826',
       },
       fontFamily: {
-        'airstrike': ['Airstrike Academy'],
-        'alata': ['Alata', 'sans-serif']
+        airstrike: ['Airstrike Academy'],
+        alata: ['Alata', 'sans-serif'],
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     styled: true,
     themes: true,
@@ -28,7 +28,30 @@ module.exports = {
     utils: true,
     logs: true,
     rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+    prefix: '',
+    darkTheme: 'dark',
+    themes: [
+      {
+        pro: {
+          primary: '#245076',
+          secondary: '#D926A9',
+          accent: '#1FB2A6',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+      {
+        dark: {
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          primary: '#245076',
+          'primary-focus': '#184062',
+          success: '#008887'
+        },
+      },
+    ],
   },
 }
